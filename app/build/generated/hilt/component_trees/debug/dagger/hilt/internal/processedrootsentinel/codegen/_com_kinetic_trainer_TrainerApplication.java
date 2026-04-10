051f0c81382564e1fa1b01141263d36e9b3b5815
Preetham V <badboy1592004@gmail.com>
2026-04-10 16:18:36 +0530

    chore: stop tracking generated gradle and build artifacts

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_kinetic_trainer_TrainerApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.kinetic.trainer.TrainerApplication"
-)
-public final class _com_kinetic_trainer_TrainerApplication {
-}
